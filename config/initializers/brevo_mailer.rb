c4cca1ec28d1e54953e05a8a950603d9f393149e
chirag-2809 <cchirag2809@gmail.com>
2026-05-13 13:19:00 +0530

    Update brevo_mailer.rb

--- config/initializers/brevo_mailer.rb
+++ config/initializers/brevo_mailer.rb
@@ -1,14 +1,13 @@
-if ENV['BREVO_API_KEY'].present?
-  require_relative '../../lib/brevo_delivery_method'
+require_relative '../../lib/brevo_delivery_method'
 
-  ActionMailer::Base.add_delivery_method(
-    :brevo,
-    BrevoDeliveryMethod,
-    api_key: ENV['BREVO_API_KEY'],
-    from_email: ENV['SMTP_FROM']
-  )
+ActionMailer::Base.add_delivery_method(
+  :brevo,
+  BrevoDeliveryMethod,
+  api_key: ENV['BREVO_API_KEY'].to_s,
+  from_email: ENV['SMTP_FROM'].to_s
+)
 
-  Rails.application.config.action_mailer.delivery_method = :brevo
-  
-  Rails.logger.info "Brevo HTTP delivery method configured"
-end
+ActionMailer::Base.delivery_method = :brevo
+Rails.application.config.action_mailer.delivery_method = :brevo
+
+Rails.logger.info "[Brevo] Delivery method set. Key starts with: #{ENV['BREVO_API_KEY'].to_s.first(15)}..."
